1d400b0fb5110fe7291d3612a7cfc2f4e6222b5c
chinadealer <chinadealer31@gmail.com>
2025-02-23 21:27:48 +0200

    Delete comments in hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 print('Hello World')
 print('One more time')
 print('Hello World')
-<<<<<<< HEAD
-=======
 print('One more time')
 print('Hello World')
->>>>>>> c7f802d53611d9db456c17203354107462a41b15
